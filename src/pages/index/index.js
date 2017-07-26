3043305b7703b693cf98ee4d303a3d4f7d228d6c
ZhouBin <jobn_joy@163.com>
2017-07-26 12:52:36 +0800

    ngrok http

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,43 +5,7 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    decks: [{
-      title: 'Arena 1',
-      place: 'Goblin Stadium',
-      img: '../../images/arenas_1.png'
-    }, {
-      title: 'Arena 2',
-      place: 'Goblin Stadium',
-      img: '../../images/arenas_2.png'
-      }, {
-        title: 'Arena 3',
-        place: 'Goblin Stadium',
-        img: '../../images/arenas_3.png'
-    }, {
-      title: 'Arena 4',
-      place: 'Goblin Stadium',
-      img: '../../images/arenas_4.png'
-      }, {
-        title: 'Arena 5',
-        place: 'Goblin Stadium',
-        img: '../../images/arenas_5.png'
-    }, {
-      title: 'Arena 6',
-      place: 'Goblin Stadium',
-      img: '../../images/arenas_6.png'
-      }, {
-        title: 'Arena 7',
-        place: 'Goblin Stadium',
-        img: '../../images/arenas_7.png'
-    }, {
-      title: 'Arena 8',
-      place: 'Goblin Stadium',
-      img: '../../images/arenas_8.png'
-      }, {
-        title: 'Arena 9',
-        place: 'Goblin Stadium',
-        img: '../../images/arenas_9.png'
-      }]
+    decks: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -52,6 +16,17 @@ Page({
   onLoad: function () {
     console.log('onLoad')
     var that = this
+    wx.request({
+      url: 'https://2d764e57.ngrok.io/api/decklists/?format=json',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        that.setData({
+          decks: res.data
+        })
+      }
+    })
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
